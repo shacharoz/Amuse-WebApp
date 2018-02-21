0d0e453accb48f7df1f973dc7fbb66cf386c11de
ohadasulin <ohad.asulin@gmail.com>
2018-02-21 19:10:34 +0200

    added patients API and changed JS to dynamically load patients to page

--- amuse-barcelona/assets/amuse-data/patientsTranslator.js
+++ amuse-barcelona/assets/amuse-data/patientsTranslator.js
@@ -1,114 +1,55 @@
-var obj = JSON.parse('{    "title": "therapy title"        ,"description": "therapy description therapy description therapy description therapy description therapy description"       ,"time": "1 Hour"       ,"main_image_path": "https://unsplash.it/700/400?image=610"       ,"activity": "0"      ,"response": "0"      ,"sensor_image_path": "https://unsplash.it/700/400?image=610"             }');
-
-
-
-var GetAllPatientsData = function() {
-
-    return [
-
-        {
-            "fullname": "Roni Benizri",
-            "image_path": "http://placehold.it/100x100",
-            "gender": "male",
-            "age": "34",
-            "address" : "sdfsdfsd",
-            "country": "england",
-            "history": "a long text of history",
-            "progress": {
-                // a lot of data about previous programs and accomplishments
-            }
-            , "link": ""             
-        }
-        ,
-        {
-            "fullname": "Zvia Elgali",
-            "image_path": "http://placehold.it/100x100",
-            "gender": "male",
-            "age": "34",
-            "address" : "sdfsdfsd",
-            "country": "england",
-            "history": "a long text of history",
-            "progress": {
-                // a lot of data about previous programs and accomplishments
-            }
-             , "link": ""             
-       }
-        , 
-        {
-            "fullname": "Moni Menizri",
-            "image_path": "http://placehold.it/100x100",
-            "gender": "male",
-            "age": "34",
-            "address" : "sdfsdfsd",
-            "country": "england",
-            "history": "a long text of history",
-            "progress": {
-                // a lot of data about previous programs and accomplishments
-            }
-             , "link": ""             
-       }
-        ,
-        {
-            "fullname": "Soni Senizri",
-            "image_path": "http://placehold.it/100x100",
-            "gender": "male",
-            "age": "34",
-            "address" : "sdfsdfsd",
-            "country": "england",
-            "history": "a long text of history",
-            "progress": {
-                // a lot of data about previous programs and accomplishments
-            }
-              , "link": ""             
-      }
-    ];
-};
-
-
-
-var GenerateItems_Patients = function() {
-
-    var _patients = GetAllPatientsData();
-
-    //$("#resultHtmlText").html("will be replaced with something");
-
+function formatHtmlFromPatientList(_patients) {
     var allItemsInHtml = "<div class='row'>";
 
-   
 
     //generate items
     var currentItemHtmlTemplate = "";
-            
-    
+
+
     for (var i = 0; i < _patients.length; i++) {
         var currentItemData = _patients[i];
 
-        //template for image based item 
-        
+        //template for image based item
+
         currentItemHtmlTemplate = "<div class='col-4'>";
         currentItemHtmlTemplate += "<a class='list-group-item list-group-item-action' href='programs.html'>";
         currentItemHtmlTemplate += "<div class='media'> ";
-        currentItemHtmlTemplate += "<img class='d-flex mr-3 rounded-circle' src=" + currentItemData.image_path +" alt=''>";
+        currentItemHtmlTemplate += "<img class='d-flex mr-3 rounded-circle' src=" + currentItemData.image_path + " alt=''>";
         currentItemHtmlTemplate += "<div class='media-body'>";
         currentItemHtmlTemplate += "<strong>" + currentItemData.fullname + "</strong>";
         currentItemHtmlTemplate += "</div></div></a></div>";
-        
+
         allItemsInHtml += currentItemHtmlTemplate;
     }
 
 
     //close the row
-     allItemsInHtml += "</div>";
-    
-    
-    //write html to gui
-    return allItemsInHtml;
+    allItemsInHtml += "</div>";
+
+
+    document.getElementById("paients_data_holder").innerHTML = allItemsInHtml;
+}
+
+var GetAllPatientsData = function() {
+    const serverRespone = {};
+
+    $.get("/patient", function (data,status) {
+        if (status !== "success") {
+            console.log("Error while updating patients data from server. status:" + status);
+        } else {
+            var _patients = data;
+
+            //$("#resultHtmlText").html("will be replaced with something");
+            formatHtmlFromPatientList(_patients);
+
+            // //write html to gui
+            // return allItemsInHtml;
+
+        }
+    });
+
 };
 
+GetAllPatientsData();
 
 
-
-var resultHtml = GenerateItems_Patients();
-
-
-document.getElementById("paients_data_holder").innerHTML = resultHtml;
