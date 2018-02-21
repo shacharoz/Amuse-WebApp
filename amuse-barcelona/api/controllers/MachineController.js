dff48719d6dd5cb8b8e67a6bd74f5562f4d2a87c
ohadasulin <ohad.asulin@gmail.com>
2018-02-21 19:01:14 +0200

    added some APIs

---
+++ amuse-barcelona/api/controllers/MachineController.js
@@ -0,0 +1,11 @@
+/**
+ * MachineController
+ *
+ * @description :: Server-side logic for managing machines
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
