9d9541e37ca7f9634a7fbdce89677365ca5805fd
shachar oz <shachar.oz@gmail.com>
2018-02-21 21:11:32 +0200

    therapy added

--- amuse-barcelona/assets/amuse-data/therapyTranslator.js
+++ amuse-barcelona/assets/amuse-data/therapyTranslator.js
@@ -1,4 +1,4 @@
-function formatHtmlFromTherapyList(_programs) {
+function formatHtmlFromTherapy(step_data) {
     var allItemsInHtml = "<div class='row'>";
 
 
@@ -6,19 +6,22 @@ function formatHtmlFromTherapyList(_programs) {
     var currentItemHtmlTemplate = "";
 
 
-    for (var i = 0; i < _programs.length; i++) {
-        var currentItemData = _programs[i];
+    for (var i = 0; i < step_data.length; i++) {
+        var currentItemData = step_data[i];
 
         //template for image based item
 
         currentItemHtmlTemplate = "<div class='col-4'>";
-        currentItemHtmlTemplate += "<a class='list-group-item list-group-item-action' href='programs.html'>";
-        currentItemHtmlTemplate += "<div class='media'> ";
-        currentItemHtmlTemplate += "<img class='d-flex mr-3 rounded-circle' src=" + currentItemData.image_path + " alt=''>";
-        currentItemHtmlTemplate += "<div class='media-body'>";
-        currentItemHtmlTemplate += "<strong>" + currentItemData.fullname + "</strong>";
+        currentItemHtmlTemplate += " <a href='program.html?id="+currentItemData.id+"&step=1' class='list-group-item list-group-item-action'>";
+        currentItemHtmlTemplate += "<div class='card-body'>";
+        currentItemHtmlTemplate += "<h6 class='card-title mb-1'>"+currentItemData.title+"</h6>";
+        currentItemHtmlTemplate += "</div><hr class='my-0'><div class='card-body'>";
+        currentItemHtmlTemplate += "<p class='card-text small'>"+ currentItemData.description+"</p></div>";
+        currentItemHtmlTemplate += "<div class='card-body py-2 small'><i class='fa fa-fw fa-thumbs-up'></i>1 Hour</div>";
+        currentItemHtmlTemplate += "<div class='col-md-3'><div class='card mb-4'>";
+        currentItemHtmlTemplate += "<img class='' src='"+currentItemData.main_image_path+"' alt=''>";
         currentItemHtmlTemplate += "</div></div></a></div>";
-
+        
         allItemsInHtml += currentItemHtmlTemplate;
     }
 
@@ -27,10 +30,37 @@ function formatHtmlFromTherapyList(_programs) {
     allItemsInHtml += "</div>";
 
 
-    document.getElementById("programs_data_holder").innerHTML = allItemsInHtml;
+    document.getElementById("program_step_data_holder").innerHTML = allItemsInHtml;
 }
 
-var GetAllProgramsData = function() {
+
+var GetSpeicificStep = function(program_id, step_index) {
+    
+    //console.log("data: "+program_id + " "+ step_index);
+    
+    var _step = 
+       {
+            "title": "program title"
+            , "description" : "program description program description program description"
+            , "main_image_path": "http://placehold.it/200x200"
+            , "sensor_system_image": "http://placehold.it/200x200"
+            , "activity" : "activity activity activity activity activity activity "
+            , "response" : "response response response response response response "
+            , "values" : {
+                "physical_therapy" : "some text here"
+                , "occupational_therapy" : ""
+                , "speach_language_therapy" : "some text here"
+                , "music_therapy" : ""
+            }
+            , "activate_link" : "c://path_to_file.lnk" 
+            , "id": 1
+       };
+    
+    
+    formatHtmlFromTherapy(_step);
+    
+    
+    /*
     const serverRespone = {};
 
     $.get("/programs", function (data,status) {
@@ -42,9 +72,18 @@ var GetAllProgramsData = function() {
             formatHtmlFromTherapyList(_programs);
         }
     });
-
+    */
 };
 
-GetAllProgramsData();
+
+
+
+
+var page_parameters = new URL(window.location.href);
+var _program_id = page_parameters.searchParams.get("id");
+var _step_index = page_parameters.searchParams.get("step");
+
+
+GetSpeicificStep(_program_id, _step_index);
 
 
