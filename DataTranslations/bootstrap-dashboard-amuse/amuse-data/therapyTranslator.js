f22a8f3638ccc979b59e80bfe415a2e27b0372fe
shachar oz <shachar.oz@gmail.com>
2018-02-21 16:28:17 +0200

    aa

--- DataTranslations/bootstrap-dashboard-amuse/amuse-data/therapyTranslator.js
+++ DataTranslations/bootstrap-dashboard-amuse/amuse-data/therapyTranslator.js
@@ -2,64 +2,110 @@ var obj = JSON.parse('{    "title": "therapy title"        ,"description": "ther
 
 
 
+var GetAllPatientsData = function() {
 
-function GeneratePortfolioItems(_items) 
-    {
-     $("#resultHtmlText").html("will be replaced with something");
-            
-        var allItemsInHtml="";
-        
-        //add pre html code to prepare the slider header
-        allItemsInHtml += "<ul class='media-list'> ";
-        
-        
-        //generate items
-        var currentItemHtmlTemplate="";
-        
-        for (var i=0; i< _items.length; i++) 
+    return [
+
         {
-            var currentItemData = _items[i];
-           
-            if (TemplateType == "ImagePullLeft") 
-            {
-                //template for image based item 
-                currentItemHtmlTemplate="<li class='media'> ";
-                currentItemHtmlTemplate += "<a class='pull-left' href='"+currentItemData.link+"' target='_blank'> ";
-                currentItemHtmlTemplate += "<img class='media-object' src='"+currentItemData.image+"'></a> ";
-                currentItemHtmlTemplate += "<div class='media-body'> ";
-                currentItemHtmlTemplate += "<h4 class='media-heading'><a class='pull-left' href='"+currentItemData.link+"' target='_blank'>"+currentItemData.title+ "</a></h4> ";
-                currentItemHtmlTemplate += currentItemData.description+"<br>";
-                
-                for (var i=0; i< currentItemData.extraLinks.length; i++) 
-                {
-                    var ExtraLink= currentItemData.extraLinks[i];
-                    currentItemHtmlTemplate += "[<a href='"+ExtraLink.link+"' target='_blank'>"+ExtraLink.description+"</a>] ";
-                }
-                
-                currentItemHtmlTemplate += "</div></li>";
+            "fullname": "roni benizri",
+            "image_path": "http://www.images.com/roni.jpg",
+            "gender": "male",
+            "age": "34",
+            "address" : "sdfsdfsd",
+            "country": "england",
+            "history": "a long text of history",
+            "progress": {
+                // a lot of data about previous programs and accomplishments
             }
-            
-            allItemsInHtml += currentItemHtmlTemplate;
         }
-    
-        //close entire ul
-         allItemsInHtml += "</ul>";
-        
-        //write html to gui
-        $("#resultHtmlText").html(allItemsInHtml);
+        ,
+        {
+            "fullname": "roni benizri",
+            "image_path": "http://www.images.com/roni.jpg",
+            "gender": "male",
+            "age": "34",
+            "address" : "sdfsdfsd",
+            "country": "england",
+            "history": "a long text of history",
+            "progress": {
+                // a lot of data about previous programs and accomplishments
+            }
+        }
+        , 
+        {
+            "fullname": "roni benizri",
+            "image_path": "http://www.images.com/roni.jpg",
+            "gender": "male",
+            "age": "34",
+            "address" : "sdfsdfsd",
+            "country": "england",
+            "history": "a long text of history",
+            "progress": {
+                // a lot of data about previous programs and accomplishments
+            }
+        }
+        ,
+        {
+            "fullname": "roni benizri",
+            "image_path": "http://www.images.com/roni.jpg",
+            "gender": "male",
+            "age": "34",
+            "address" : "sdfsdfsd",
+            "country": "england",
+            "history": "a long text of history",
+            "progress": {
+                // a lot of data about previous programs and accomplishments
+            }
+        }
+    ];
+};
+
+
+
+var GenerateItems_Patients = function() {
+
+    var _patients = GetAllPatientsData();
+
+    $("#resultHtmlText").html("will be replaced with something");
+
+    var allItemsInHtml = "";
+
+    //add pre html code to prepare the slider header
+    allItemsInHtml += "<ul class='media-list'> ";
+
+
+    //generate items
+    var currentItemHtmlTemplate = "";
+
+    for (var i = 0; i < _patients.length; i++) {
+        var currentItemData = _patients[i];
+
+        if (TemplateType == "ImagePullLeft") {
+            //template for image based item 
+            currentItemHtmlTemplate = "<li class='media'> ";
+            currentItemHtmlTemplate += "<a class='pull-left' href='" + currentItemData.fullname + "' target='_blank'> ";
+            currentItemHtmlTemplate += "<img class='media-object' src='" + currentItemData.image_path + "'></a> ";
+            currentItemHtmlTemplate += "<div class='media-body'> ";
+            currentItemHtmlTemplate += "<h4 class='media-heading'><a class='pull-left' href='" + currentItemData.link + "' target='_blank'>" + currentItemData.title + "</a></h4> ";
+           
+            currentItemHtmlTemplate += "</div></li>";
+        }
+
+        allItemsInHtml += currentItemHtmlTemplate;
     }
-    
+
+    //close entire ul
+    allItemsInHtml += "</ul>";
+
+    //write html to gui
+    return allItemsInHtml;
+};
 
 
 
 
-
-
-var resultHtml = "";
+var resultHtml = GenerateItems_Patients();
 
 //obj.title + " , "+ obj.description + " , "+ obj.time; 
 
 document.getElementById("demo").innerHTML = resultHtml;
-
-
-
