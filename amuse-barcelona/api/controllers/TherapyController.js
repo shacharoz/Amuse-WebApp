3e2c4141ef33fab18bb8cc0fe6ef5b1da8cb0b96
ohadasulin <ohad.asulin@gmail.com>
2018-02-24 22:34:26 +0200

    added missing models

---
+++ amuse-barcelona/api/controllers/TherapyController.js
@@ -0,0 +1,11 @@
+/**
+ * TherapyController
+ *
+ * @description :: Server-side logic for managing therapies
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
